476997584a236af8aad13e6262139014f46539d2
donghyun kim <donghyun1998@naver.com>
2023-12-04 01:10:55 +0900

    fix: header 상단에 고정시킴

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,17 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6>
+      <h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6>v<h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6><h6 className=''>홈 화면</h6>
     </div>
   )
 }
