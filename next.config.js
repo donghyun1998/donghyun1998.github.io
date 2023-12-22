ed19b00640331818253f4fbf10e3ea6bc7673058
김동현 <donghyun@gimdonghyeon-ui-MacBookPro.local>
2023-12-22 17:28:59 +0900

    build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const { withContentlayer } = require('next-contentlayer')
 
-/** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true, swcMinify: true }
 
+/** @type {import('next').NextConfig} */
+const prod = process.env.NODE_ENV === 'production'
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  ...(prod && { output: 'export' }),
+}
 module.exports = withContentlayer(nextConfig)
