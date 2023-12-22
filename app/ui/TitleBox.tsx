c61d64d388a817d92b12fc3a928b038db381b827
김동현 <donghyun@gimdonghyeon-ui-MacBookPro.local>
2023-12-22 16:58:36 +0900

    add: titlebox hover 추가

--- app/ui/TitleBox.tsx
+++ app/ui/TitleBox.tsx
@@ -1,12 +1,18 @@
+'use client'
 import { Post } from '@/.contentlayer/generated'
 import { format } from 'date-fns'
 import Link from 'next/link'
 
 export default function TitleBox(post: Post) {
   return (
-    <div className=" mt-4 flex overflow-hidden rounded-md shadow-md">
+    <div
+      className="mt-4 flex cursor-pointer overflow-hidden rounded-md shadow-md hover:bg-sky-300"
+      onClick={() => (window.location.href = post.url)}
+    >
       <div className="p-2">
-      <a className="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">{post.tag}</a>
+        <a className="me-2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300">
+          {post.tag}
+        </a>
         <Link href={post.url} className="mb-2 text-xl font-bold">
           {post.title}
         </Link>
