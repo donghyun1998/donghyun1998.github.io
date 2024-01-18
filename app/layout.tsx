bddd83e530332c1d6d47a06303848013017d23b3
donghyun <donghyun1998@naver.com>
2024-01-18 16:12:05 +0900

    chore: 블로그 설명 head에 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import Navbar from '@/app/ui/Navbar'
+import Head from 'next/head'
 import Link from 'next/link'
 import './globals.css'
-import Navbar from '@/app/ui/Navbar'
 
 const Header = () => {
   return (
@@ -8,7 +9,7 @@ const Header = () => {
       <h1 className="flex items-center text-4xl font-bold">
         <Link href="/">대단한 동현 블로그</Link>
       </h1>
-      <Navbar/>
+      <Navbar />
     </header>
   )
 }
@@ -19,11 +20,15 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="ko">
-      <body className="flex h-screen w-screen flex-col">
-        <Header />
-        <div className="flex h-full w-screen overflow-y-scroll">{children}</div>
-      </body>
-    </html>
+    <>
+      <Head>
+      <title>대단한 동현 블로그</title>
+        <meta name="description" content="동현의 멋진 블로그에 오신 것을 환영합니다." />
+        <meta name="keywords" content="동현, 블로그, 개발" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Header />
+      <div className="flex h-full w-screen overflow-y-scroll">{children}</div>
+    </>
   )
 }
