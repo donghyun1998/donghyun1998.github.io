6548ba9749a2be79b6fbbe556cc1e97a47723e49
donghyun <donghyun1998@naver.com>
2024-03-28 21:21:35 +0900

    feat: 오른쪽 튀는거 일단 overflow-x로 임시로 막음

--- app/ui/Nav-bar.tsx
+++ app/ui/Nav-bar.tsx
@@ -44,28 +44,9 @@ export default function Navbar() {
                 </Link>
               </li>
             ))}
-            {/* <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li> */}
           </ul>
         </div>
       </div>
     </nav>
-
-    // <header className="flex h-20 min-h-max w-full min-w-max space-x-8 p-4">
-    //   <h1 className="flex items-center text-4xl font-bold">
-    //     <Link href="/">대단한 동현 블로그</Link>
-    //   </h1>
-    //   <Navbar/>
-    // </header>
   )
 }
