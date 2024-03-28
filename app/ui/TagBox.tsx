43dd817f1351677f8316665b75be9b9b975eac2b
donghyun <donghyun1998@naver.com>
2024-03-28 21:13:38 +0900

    feat: tagbox titlebox 예쁘게

--- app/ui/TagBox.tsx
+++ app/ui/TagBox.tsx
@@ -11,10 +11,12 @@ interface Props {
 export default function TagBox({ tag, count, category }: Props) {
   const { push } = useRouter()
   return (
-    <Badge
-      tag={tag}
-      count={count}
+    <span
       onClick={() => push(`/${category}?tag=${tag}`)}
-    />
+      className="me-2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 hover:bg-blue-200 dark:bg-blue-900  dark:text-blue-300 dark:hover:bg-blue-500"
+      style={{ cursor: 'pointer' }}
+    >
+      {tag} ({count})
+    </span>
   )
 }
--- app/ui/TitleBox.tsx
+++ app/ui/TitleBox.tsx
@@ -6,17 +6,17 @@ import Link from 'next/link'
 export default function TitleBox(post: Post) {
   return (
     <div
-      className="mt-4 flex cursor-pointer overflow-hidden rounded-md shadow-md hover:bg-sky-300"
+      className="mt-4 flex cursor-pointer overflow-hidden rounded-md shadow-md hover:bg-sky-700"
       onClick={() => (window.location.href = post.url)}
     >
       <div className="p-2">
-        <a className="me-2 rounded bg-blue-100 px-2.5 py-0.5 text-sm font-medium text-blue-800 dark:bg-blue-900 dark:text-blue-300">
+        <span className="me-2 rounded bg-blue-900 px-2.5 py-0.5 text-sm font-medium text-blue-300">
           {post.tag}
-        </a>
-        <Link href={post.url} className="mb-2 text-xl font-bold">
+        </span>
+        <Link href={post.url} className="mb-2 text-xl font-bold text-white">
           {post.title}
         </Link>
-        <div style={{ color: 'gray', fontSize: 'smaller' }}>
+        <div style={{ fontSize: 'smaller' }}>
           {format(new Date(post.date), 'yyyy년 MM월 dd일')}
         </div>
       </div>
