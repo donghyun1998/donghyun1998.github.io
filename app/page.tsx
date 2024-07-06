4bce605f53d823f242d5e3be311aae754b7c87e9
donghyun <donghyun1998@naver.com>
2024-07-06 23:45:41 +0900

    feat: 블로그 링크 남기기 ㅋ

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex h-full w-full items-center justify-center text-4xl">
-      <div>Hello World!</div>
+      <span>더 대단한</span>
+      <Link className="ml-4 text-sky-400" href="https://donghyk2.tistory.com/">
+        동현 블로그
+      </Link>
+      <span>로 오세요</span>
     </main>
   )
 }
